a21379a2979301c1f78707e2fd75f163639bf71e
EpsilonYega <amd7tashit@gmail.com>
2024-10-18 16:08:27 +0300

    v1.2

--- src/main/java/com/parser/ytparser/service/YouTubeService.java
+++ src/main/java/com/parser/ytparser/service/YouTubeService.java
@@ -33,8 +33,6 @@ public class YouTubeService {
 
     public List<ChannelInfo> searchChannels(String query, BigDecimal minSubscribers) {
         List<ChannelInfo> channels = new ArrayList<>();
-
-        // 1. Выполните запрос к SEARCH_URL с параметром q=query
         String searchUrl = UriComponentsBuilder.fromHttpUrl(SEARCH_URL)
                 .queryParam("part", "snippet")
                 .queryParam("q", query)
@@ -46,13 +44,11 @@ public class YouTubeService {
         YouTubeSearchResponse searchResponse = restTemplate.getForObject(searchUrl, YouTubeSearchResponse.class);
 
         if (searchResponse != null && searchResponse.getItems() != null) {
-            // 2. Получите ID каналов из ответа
             List<String> channelIds = new ArrayList<>();
             for (YouTubeSearchItem item : searchResponse.getItems()) {
                 channelIds.add(item.getId().getChannelId());
             }
 
-            // 3. Выполните запрос к CHANNEL_URL для получения информации о каналах
             String channelIdsParam = String.join(",", channelIds);
             String channelUrl = UriComponentsBuilder.fromHttpUrl(CHANNEL_URL)
                     .queryParam("part", "statistics,snippet")
@@ -63,7 +59,6 @@ public class YouTubeService {
 
             YouTubeChannelResponse channelResponse = restTemplate.getForObject(channelUrl, YouTubeChannelResponse.class);
 
-            // 4. Фильтруйте каналы по количеству подписчиков
             if (channelResponse != null && channelResponse.getItems() != null) {
                 for (YouTubeChannelItem channelItem : channelResponse.getItems()) {
                     BigDecimal subscriberCount = channelItem.getStatistics().getSubscriberCount();
@@ -72,7 +67,6 @@ public class YouTubeService {
                         channelInfo.setChannelName(channelItem.getSnippet().getTitle());
                         channelInfo.setChannelUrl("https://www.youtube.com/channel/" + channelItem.getId());
                         channelInfo.setSubscriberCount(subscriberCount);
-                        // Добавьте другие параметры, такие как количество видео, язык и т.д.
                         channels.add(channelInfo);
                     }
                 }
